bc13a3d81bc3464091fda5b6d9ef3835a14e5499
mineArrow <t.iwai014@gmail.com>
2017-05-20 12:49:01 +0900

    first commmit

---
+++ src/main/java/jp/co/aimsoft/SpringBootMybatisPostgresApplication.java
@@ -0,0 +1,12 @@
+package jp.co.aimsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMybatisPostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMybatisPostgresApplication.class, args);
+	}
+}
